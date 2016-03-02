a6fbf956b09426665961e8a654391eb4874ebacb
Dmitriy Kapelushniy <test_git@mail.ru>
2016-03-02 18:27:20 +0200

    First commit

---
+++ src/main/java/com/io_properties_test/TestOperatingProperties.java
@@ -0,0 +1,80 @@
+package com.io_properties_test;
+
+
+
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Scanner;
+
+
+public class TestOperatingProperties {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("");
+        File propertyFile = new File(path.toAbsolutePath() + "/properties.txt");
+        Properties properties = System.getProperties();
+        File dataFile = new File(path.toAbsolutePath() + "/" + properties.getProperty("user.name") + ".txt");
+        if(!dataFile.exists()){
+            dataFile.createNewFile();
+        }
+
+        String[] propertiesArray = getPropertiesFromFile(propertyFile);
+        String[] propertiesVariables = getConfigValues(propertiesArray);
+
+        FileWriter fileWriter = new FileWriter(dataFile, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        for (String s : propertiesVariables) {
+            bufferedWriter.write(s + "\n");
+            bufferedWriter.flush();
+        }
+
+        System.out.println(Arrays.toString(propertiesArray));
+        System.out.println(Arrays.toString(propertiesVariables));
+
+
+
+    }
+
+    private static String[] getConfigValues(String[] propertiesArray) throws IOException {
+        Properties properties = new Properties();
+        InputStream resourceAsStream = TestOperatingProperties.class.getClassLoader().getResourceAsStream("configFiles/config.properties");
+        properties.load(resourceAsStream);
+
+        String[] propertiesVariables = new String[propertiesArray.length];
+
+        int i = 0;
+        for (String s : propertiesArray) {
+            propertiesVariables[i] = s + " is " + properties.getProperty(s, "not available");
+            ++i;
+        }
+        return propertiesVariables;
+    }
+
+    private static String[] getPropertiesFromFile(File propertyFile) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(propertyFile);
+        int wordsNumberInFile = 0;
+        Scanner scan = new Scanner(propertyFile);
+
+        do{
+            scan.nextLine();
+            ++wordsNumberInFile;
+        }while(scan.hasNext());
+        scan.close();
+
+        String[] propertiesArray = new String[wordsNumberInFile];
+        Scanner newScan = new Scanner(fileInputStream);
+        for (int i = 0; i < wordsNumberInFile ; i++) {
+            propertiesArray[i] = newScan.nextLine();
+        }
+        newScan.close();
+        return propertiesArray;
+    }
+
+
+}
